fcfb784f5120ebad25f47247e167d7c955f6ea8c
MicheleMeloni29 <melonimichelee29@gmail.com>
2026-01-13 10:29:59 +0100

    Add Vercel analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from './components/header';
 import Providers from './provider';
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata = {
   title: 'Michele Meloni',
@@ -10,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="h-ful overflow-x-hidden">
+      <body className="h-full overflow-x-hidden">
         <Providers>
           <div className="relative z-10 flex flex-col h-screen">
             <header className="sticky top-0 z-20">
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <main className="flex-1 overflow-y-auto">{children}</main>
           </div>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
